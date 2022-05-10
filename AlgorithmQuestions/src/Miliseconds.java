313ea8821168924dce6e9ce2108b791badf5117e
owermaiden <ower.maiden@cybertekschool.com>
2022-05-10 17:55:37 +0300

    Miliseconds convertion

---
+++ AlgorithmQuestions/src/Miliseconds.java
@@ -0,0 +1,14 @@
+public class Miliseconds {
+    public static void main(String[] args) {
+
+        long milliseconds = 39504567;
+
+        int seconds = (int) (milliseconds / 1000) % 60 ;
+
+        int minutes = (int) ((milliseconds / (1000*60)) % 60);
+
+        int hours   = (int) ((milliseconds / (1000*60*60)) % 24);
+
+        System.out.println(milliseconds + " Milliseconds = " + hours + " Hours, " + minutes + " minutes, " + seconds + " seconds");
+    }
+}
