2b6362ace176d1ef7cb2bfc95fd773ef432615a6
owermaiden <ower.maiden@cybertekschool.com>
2022-05-24 09:54:27 +0300

    animalFeast

---
+++ AlgorithmQuestions/src/AnimalFeast.java
@@ -0,0 +1,14 @@
+public class AnimalFeast {
+    public static void main(String[] args) {
+        String animal = "hello";
+        String dish = "hayırdıro";
+
+        System.out.println(isEligible(animal, dish));
+    }
+
+    public static boolean isEligible(String animal, String dish){
+
+        return (animal.charAt(0) +""+ animal.charAt(animal.length() - 1)).equals(dish.charAt(0)+""+dish.charAt(dish.length() - 1));
+
+    }
+}
