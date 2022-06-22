6ae6a9376a406b783ace2fdcf0ad581b5b92ef03
owermaiden <ower.maiden@cybertekschool.com>
2022-06-22 17:04:12 +0300

    palindrome

---
+++ AlgorithmQuestions/src/Palindrome.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("carac"));
+        System.out.println(getPalindromes(new String[]{"hi", "myym", "carra", "palilap"}));
+    }
+
+    static ArrayList<String> getPalindromes(String[] array){
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String s : array) {
+            //call the method you have created for question-1
+            if(isPalindrome(s)){
+             result.add(s);
+             }
+        }
+        return result;
+    }
+
+    static boolean isPalindrome(String str){
+
+        if (str.isBlank()){
+            return true;
+        }
+
+        str = str.toLowerCase().replace(" ", "");
+        int strLength = str.length();
+        for (int i = 0, j = strLength-1; i < strLength/2; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
