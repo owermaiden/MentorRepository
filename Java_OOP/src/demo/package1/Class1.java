abcb21ebe23febbef4e167dce5fb35d8f4ca76a1
owermaiden <ower.maiden@cybertekschool.com>
2022-06-27 01:19:29 +0300

    OOP examples

---
+++ Java_OOP/src/demo/package1/Class1.java
@@ -0,0 +1,13 @@
+package demo.package1;
+
+public class Class1 {
+
+    public int x;
+    protected int y;
+    int z;
+    private int u;
+
+    protected void m(){
+
+    }
+}
---
+++ Java_OOP/src/demo/package1/Class3.java
@@ -0,0 +1,22 @@
+package demo.package1;
+
+public class Class3 {
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+
+//        System.out.println(class1.x);
+//        System.out.println(class1.y);
+//        System.out.println(class1.z);
+//        System.out.println(class1.u);
+//        class1.m();
+//
+//        Class3 class3 = new Class3();
+//        System.out.println(class3.x);
+//        System.out.println(class3.y);
+//        System.out.println(class3.z);
+//        System.out.println(class3.u);
+//        class3.m();
+
+    }
+}
