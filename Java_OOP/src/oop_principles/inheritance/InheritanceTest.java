cb082c3ccafbe53e6e4c4d54e51b557186fa6837
owermaiden <ower.maiden@cybertekschool.com>
2022-06-29 01:43:24 +0300

    inheritance

---
+++ Java_OOP/src/oop_principles/inheritance/InheritanceTest.java
@@ -0,0 +1,9 @@
+package oop_principles.inheritance;
+
+public class InheritanceTest {
+    public static void main(String args[]) {
+
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+    }
+}
